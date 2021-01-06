18fddc784cf84238e0c04fbe16e350073a4add87
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-01-05 18:57:25 -0600

    created parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,12 @@
 CREATE TABLE parties (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT 
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
 
 CREATE TABLE candidates (
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  industry_connected BOOLEAN NOT NULL
 );
-
